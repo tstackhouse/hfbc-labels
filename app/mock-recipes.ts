8ef86d8547884f43b083e5ac9a9e75f4fa1e2de0
Tim Stackhouse <tstackhouse@gmail.com>
2016-03-22 12:41:20 -0400

    Update Reynard's Best recipe details

--- app/mock-recipes.ts
+++ app/mock-recipes.ts
@@ -16,13 +16,13 @@ export var RECIPES: Object[] = [
     {
         id: 2,
         name: 'Reynard\'s Best',
-        style: 'Fopreign Extra Stout',
+        style: 'Foreign Extra Stout',
         batchCode: 1,
-        brewDate: '30 Jan 2016',
+        brewDate: '31 Jan 2016',
         packageDate: '15 Mar 2016',
         originalGravity: 1.069,
         finalGravity: 1.029,
-        iBUs: 47,
-        yeastStrain: 'Wyeast xxxx Thames Valley Ale'
+        iBUs: 55,
+        yeastStrain: 'Wyeast 1275 Thames Valley Ale'
     }
 ];
